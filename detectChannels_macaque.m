be8894ed5db8e40ed53a4e4f7fadbccc8295feb1
Chikayo Hemmi (Monash University) <chikayoh@m3-login2.massive.org.au>
2026-01-09 18:07:58 +1100

    Add detectChannels_macaque wrapper (README compatibility)

---
+++ detectChannels_macaque.m
@@ -0,0 +1,5 @@
+function varargout = detectChannels_macaque(varargin)
+% Wrapper for README compatibility.
+% Use the existing Neurotycho (macaque) implementation.
+[varargout{1:nargout}] = detectChannels_neurotycho(varargin{:});
+end
